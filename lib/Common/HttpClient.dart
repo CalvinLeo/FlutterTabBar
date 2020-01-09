3c919fe8ce7b920b65c0e8fd9b9a38c3d1d63a4e
Calvin <349562074@qq.com>
2020-01-09 17:29:34 +0800

    update

--- lib/Common/HttpClient.dart
+++ lib/Common/HttpClient.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/rendering.dart';
 
 class HttpClient {
   static final String GET = "get";
@@ -12,7 +11,7 @@ class HttpClient {
   static HttpClient get instance => _getInstance();
   static HttpClient _instance;
   Dio dio;
-  
+
   HttpClient._internal() {
     // 初始化
     dio = Dio(BaseOptions(
@@ -29,19 +28,30 @@ class HttpClient {
     return _instance;
   }
 
-  get (String urlStr, Function successCallBack, 
-      {params, Function errorCallBask}) async {
-    _requestHttp(urlStr, successCallBack, GET, params, errorCallBask);
+  // get 请求 可选命名参数，用 {} 包裹 
+  get(
+    String urlStr, { 
+    Map<String, dynamic> params, 
+    Function successCallBack,
+    Function errorCallBask,
+  }) async {
+    _requestHttp(urlStr, GET, params, successCallBack, errorCallBask);
   }
 
-  _requestHttp (String urlStr, Function successCallBack, 
-      [String method, Map<String, dynamic> params, Function errorCallBack]) async {
-        String errorMsg = "";
-        int code;
+  // 请求方法
+  _requestHttp(
+    String urlStr,
+    String method,
+    Map<String, dynamic> params, 
+    Function successCallBack, 
+    Function errorCallBack,
+  ) async {
+    String msg = "";
+    int code;
 
-      try {
-        Response response;
-        _addStartHttpInterceptor(dio); //添加请求之前的拦截器
+    try {
+      Response response;
+      _addStartHttpInterceptor(dio); //添加请求之前的拦截器
       if (method == GET) {
         if (params != null && params.isNotEmpty) {
           response = await dio.get(urlStr, queryParameters: params);
@@ -56,48 +66,58 @@ class HttpClient {
         }
       }
 
+      // respose 处理
       code = response.statusCode;
-      if (code != 200) {
-        errorMsg = '错误码：' + code.toString() + '，' + response.data.toString();
-        _error(errorCallBack, errorMsg);
-        return;
+      msg = 'success';
+      if (code == 200) {
+        if (successCallBack != null) 
+          successCallBack(code, msg, response.data);
+      }
+      else {
+        msg = '错误码：' + code.toString() + '，' + response.data.toString();
+        if (errorCallBack != null)
+          errorCallBack(code, msg);
+        // _error(errorCallBack, errorMsg);
+        // return;
       }
 
-      String dataStr = json.encode(response.data);
-      Map<String, dynamic> dataMap = json.decode(dataStr);
-      if (dataMap != null && dataMap[CODE] != 0) {
-        errorMsg =
-            '错误码：' + dataMap[CODE].toString() + '，' + response.data.toString();
-        _error(errorCallBack, errorMsg);
-        return;
-      }
+      // String dataStr = json.encode(response.data);
+      // Map<String, dynamic> dataMap = json.decode(dataStr);
+      // if (dataMap != null && dataMap[CODE] != 0) {
+      //   errorMsg =
+      //       '错误码：' + dataMap[CODE].toString() + '，' + response.data.toString();
+      //   _error(errorCallBack, errorMsg);
+      //   return;
+      // }
 
       if (successCallBack != null) {
-        successCallBack(dataMap[DATA]);
+        // successCallBack(dataMap[DATA]);
       }
     } catch (exception) {
       _error(errorCallBack, exception.toString());
     }
   }
 
+  // 错误处理
   _error(Function errorCallBack, String error) {
-    Fluttertoast.showToast(
-        msg: error.toString(),
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.CENTER);
+    // Fluttertoast.showToast(
+    //     msg: error.toString(),
+    //     toastLength: Toast.LENGTH_SHORT,
+    //     gravity: ToastGravity.CENTER);
     if (errorCallBack != null) {
       errorCallBack(error);
     }
   }
 
+  // 拦截
   _addStartHttpInterceptor(Dio dio) {
-    dio.interceptor.request.onSend = (Options options) {
-      // 在请求被发送之前做一些事情   比如加密的一些操作 或者添加token等参数 对head 或者请求参数进行加工处理
-      Map<String, dynamic> headers = options.headers;
-      Map<String, dynamic> body = options.data;
-      /*request['token'] = '1111111111';
-      headers['addParam'] = 'aaaaaaaaaaaaaaa';*/
-      return options;
-    };
+    // dio.interceptors.request.onSend = (Options options) {
+    //   // 在请求被发送之前做一些事情   比如加密的一些操作 或者添加token等参数 对head 或者请求参数进行加工处理
+    //   Map<String, dynamic> headers = options.headers;
+    //   Map<String, dynamic> body = options.data;
+    //   /*request['token'] = '1111111111';
+    //   headers['addParam'] = 'aaaaaaaaaaaaaaa';*/
+    //   return options;
+    // };
   }
 }
--- lib/Controller/HomeVC.dart
+++ lib/Controller/HomeVC.dart
@@ -1,4 +1,6 @@
+import 'package:dbcDemo/Common/AppColor.dart';
 import 'package:flutter/material.dart';
+import 'package:dbcDemo/Common/HttpClient.dart';
 
 class HomeVC extends StatefulWidget {
   @override
@@ -8,6 +10,15 @@ class HomeVC extends StatefulWidget {
 }
 
 class HomePageState extends State<HomeVC> {
+
+  var homeTitle;
+
+  @override
+  void initState() {
+    loadData();
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +28,31 @@ class HomePageState extends State<HomeVC> {
         ),
       ),
       body: Center(
-        child: Text("Im, HomeVC"),
+        child: Text('Im, my content $homeTitle',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16, color:AppColors.black_333),
+        ),
       ),
     );   
+  }
+
+  // 加载请求
+  loadData() async {
+    Map<String, dynamic> params = {
+      "os": "ios",
+    };
+    HttpClient httpclient = HttpClient.instance;
+    await httpclient.get("http://baidu.com", 
+      params: params,
+      successCallBack: (int code, String msg, dynamic data) {
+        print("请求成功：" + code.toString() + "\n" + "请求信息" + msg + "\n" + "数据" + data);
+        setState(() {
+          this.homeTitle = data;
+        });
+      },
+      errorCallBask: (int code, String msg) {
+        print("ErrCode:" + code.toString() + "\n" + "Errmsg:" + msg);
+      },
+    );
   }
 }
